bb41f0d8a037b10dcfe1b748f7413446feb95474
Andrew Hu <andrewhu@uw.edu>
2018-10-07 23:55:01 -0700

    =formatting

--- src/LearnerModel.ts
+++ src/LearnerModel.ts
@@ -1,97 +1,94 @@
 import { MapID, MapDate} from "./Map"
 
-//export module pla {
+export class Learner {
+    constructor(public id: number, 
+            public difficultyStyle: boolean, 
+            public mindset: number, 
+            public firstName: string,
+            public lastName: string,
+            public preferredName: string,
+            public gender: string,
+            public birthdate: Date,
+            public age: number) {    }
+}
 
-    export class Learner {
-        constructor(public id: number, 
-                public difficultyStyle: boolean, 
-                public mindset: number, 
-                public firstName: string,
-                public lastName: string,
-                public preferredName: string,
-                public gender: string,
-                public birthdate: Date,
-                public age: number) {    }
-    }
+/** Which part of program tracing is being assessed */
+class Path { id : string }
+/** The series of paths up to this point */
+class PathSequence { id : string; seq : Array<Path>; }
+class Probability { prob : string }
+class UserAction { id : string }
+class SurveyQuestion {
+    // TODO: should this just be the question as a string or an id?
+    id : string
+}
+class LearnerResponse {
+    // TODO: Update with what Greg says should go in here
+    constructor(public q: SurveyQuestion) {}
+}
 
-    /** Which part of program tracing is being assessed */
-    class Path { id : string }
-    /** The series of paths up to this point */
-    class PathSequence { id : string; seq : Array<Path>; }
-    class Probability { prob : string }
-    class UserAction { id : string }
-    class SurveyQuestion {
-        // TODO: should this just be the question as a string or an id?
-        id : string
+/** timestamp is set at the time it is constructed */
+class KMUpdateRow {
+    timestamp : Date;
+    response : LearnerResponse;
+    pathBefore : MapID<Path, Probability>;
+    pathAfter  : MapID<Path, Probability>;
+    pathSeqBefore : MapID<PathSequence, Probability>;
+    pathSeqAfter  : MapID<PathSequence, Probability>;
+    constructor(_response: LearnerResponse, _pathBefore: MapID<Path, Probability>, _pathAfter: MapID<Path, Probability>,
+            _pathSeqBefore: MapID<PathSequence, Probability>, _pathSeqAfter: MapID<PathSequence, Probability>) {
+        this.timestamp = new Date();
+        this.response = _response;
+        this.pathBefore = _pathBefore;
+        this.pathAfter = _pathAfter;
+        this.pathSeqBefore = _pathSeqBefore;
+        this.pathSeqAfter = _pathSeqAfter;
     }
-    class LearnerResponse {
-        // TODO: Update with what Greg says should go in here
-        constructor(public q: SurveyQuestion) {}
-    }
+}
 
-    /** timestamp is set at the time it is constructed */
-    class KMUpdateRow {
-        timestamp : Date;
-        response : LearnerResponse;
-        pathBefore : MapID<Path, Probability>;
-        pathAfter  : MapID<Path, Probability>;
-        pathSeqBefore : MapID<PathSequence, Probability>;
-        pathSeqAfter  : MapID<PathSequence, Probability>;
-        constructor(_response: LearnerResponse, _pathBefore: MapID<Path, Probability>, _pathAfter: MapID<Path, Probability>,
-                _pathSeqBefore: MapID<PathSequence, Probability>, _pathSeqAfter: MapID<PathSequence, Probability>) {
-            this.timestamp = new Date();
-            this.response = _response;
-            this.pathBefore = _pathBefore;
-            this.pathAfter = _pathAfter;
-            this.pathSeqBefore = _pathSeqBefore;
-            this.pathSeqAfter = _pathSeqAfter;
-        }
+/** A set of KMUpdateRow's */
+class KMUpdateLog {
+    rowSet : MapDate<KMUpdateRow>;
+    addEntry (input : KMUpdateRow) {
+        this.rowSet[input.timestamp.toDateString()] = input;
     }
+}
 
-    /** A set of KMUpdateRow's */
-    class KMUpdateLog {
-        rowSet : MapDate<KMUpdateRow>;
-        addEntry (input : KMUpdateRow) {
-            this.rowSet[input.timestamp.toDateString()] = input;
-        }
+export class LearnerKnowledgeModel {
+    id : string;
+    byPath : MapID<Path, Probability>;
+    byPathSequences : MapID<PathSequence, Probability>;
+    pathPrior : MapID<Path, Probability>;
+    pathSeqPrior : MapID<PathSequence, Probability>;
+    updateLog: KMUpdateLog;
+    getPath () : MapID<Path, Probability> { return this.byPath; }
+    getPathSequences() : MapID<PathSequence, Probability> { return this.byPathSequences; }
+    setPathPrior(p: Path, prob: Probability) { this.pathPrior.set(p, prob); }
+    setPathSeqPrior(ps: PathSequence, prob: Probability) { this.pathSeqPrior[ps.id] = prob; }
+    update(answer: LearnerResponse) {
+        // create a KMUpdateRow with the input answer
+        var input = new KMUpdateRow(answer, new MapID<Path, Probability>(), new MapID<Path, Probability>(), new MapID<PathSequence, Probability>(), new MapID<PathSequence, Probability>());
+        this.updateLog.addEntry(input);
     }
-
-    export class LearnerKnowledgeModel {
-        id : string;
-        byPath : MapID<Path, Probability>;
-        byPathSequences : MapID<PathSequence, Probability>;
-        pathPrior : MapID<Path, Probability>;
-        pathSeqPrior : MapID<PathSequence, Probability>;
-        updateLog: KMUpdateLog;
-        getPath () : MapID<Path, Probability> { return this.byPath; }
-        getPathSequences() : MapID<PathSequence, Probability> { return this.byPathSequences; }
-        setPathPrior(p: Path, prob: Probability) { this.pathPrior.set(p, prob); }
-        setPathSeqPrior(ps: PathSequence, prob: Probability) { this.pathSeqPrior[ps.id] = prob; }
-        update(answer: LearnerResponse) {
-            // create a KMUpdateRow with the input answer
-            var input = new KMUpdateRow(answer, new MapID<Path, Probability>(), new MapID<Path, Probability>(), new MapID<PathSequence, Probability>(), new MapID<PathSequence, Probability>());
-            this.updateLog.addEntry(input);
-        }
+}
+        
+export class LearnerModel {
+    userActionLog : MapDate<UserAction>;
+    constructor(public learner: Learner, public knowledgeModel: LearnerKnowledgeModel) {    }
+    /** Records actions taken by this learner */
+    addUserAction (ua: UserAction) {
+        this.userActionLog[new Date().toDateString()] = ua;
     }
-            
-    export class LearnerModel {
-        userActionLog : MapDate<UserAction>;
-        constructor(public learner: Learner, public knowledgeModel: LearnerKnowledgeModel) {    }
-        /** Records actions taken by this learner */
-        addUserAction (ua: UserAction) {
-            this.userActionLog[new Date().toDateString()] = ua;
-        }
-        addSurveyAnswer (answer: LearnerResponse) {
-            this.knowledgeModel.update(answer);
-        }
-        /** What are all of the current answers to all of the questions
-         * TODO: clarify are the keys here all of the questions total, or those asked so far?
-         */
-        latestSurveyAnswers () : MapID<SurveyQuestion, string> {
-            return new MapID();
-        }
-        historicalSurveyAnswers () : MapID<SurveyQuestion, string> {
-            return new MapID();
-        }
+    addSurveyAnswer (answer: LearnerResponse) {
+        this.knowledgeModel.update(answer);
     }
-//}
+    /** What are all of the current answers to all of the questions
+     * TODO: clarify are the keys here all of the questions total, or those asked so far?
+     */
+    latestSurveyAnswers () : MapID<SurveyQuestion, string> {
+        return new MapID();
+    }
+    historicalSurveyAnswers () : MapID<SurveyQuestion, string> {
+        return new MapID();
+    }
+}
