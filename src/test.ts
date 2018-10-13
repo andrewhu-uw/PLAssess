9d3157ef54a08b3840f4dff0ceb557a38f63ac18
Andrew Hu <andrewhu@uw.edu>
2018-10-13 14:28:28 -0700

    -old tests

--- src/test.ts
+++ src/test.ts
@@ -28,19 +28,5 @@ describe("LearnerModelSchema", () => {
     })
 })
 
-function testSchemaLearnerModel() {
-    var learner = new Learner(5, true, 4, "A", "Hu", "A", "fdsf", new Date(5000), 5);
-    var lm = new LearnerModel(learner, new LearnerKnowledgeModel());
-
-    var JSONString = JSON.stringify(lm);
-    console.log(JSONString);
-    if (JSONString == '{"learner":{"id":5,"difficultyStyle":true,"mindset":4,"firstName":"A","lastName":"Hu","preferredName":"A","gender":"fdsf","birthDate":"1970-01-01T00:00:05.000Z","age":5},"knowledgeModel":{}}') {
-        console.log("PASS");
-    } else {
-        console.log("FAIL");
-    }
-}
-
-testSchemaLearnerModel();
 initDB();
 loadDB();
